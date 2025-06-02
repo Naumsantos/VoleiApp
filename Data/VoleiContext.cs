1dd1ae7097d63f326f52d3f74539fa905380b3c9
Naum Santos Mourão <naum.santos@unemat.br>
2025-06-02 00:12:37 -0300

    Adiciona as configurações do banco do projeto

---
+++ Data/VoleiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VoleiApp.Models;
+
+
+namespace VoleiApp.Data
+{
+    public class VoleiContext : DbContext
+    {
+        public VoleiContext(DbContextOptions<VoleiContext> options) : base(options) { }
+        public DbSet<Atleta> Atletas { get; set; }
+        public DbSet<Partida> Partidas { get; set; }
+    }
+}
