314ff31974dbf40c8e71a6a5499fede78efff440
Naum Santos Mourão <naum.santos@unemat.br>
2025-06-02 00:14:08 -0300

    Adiciona as configurações gerais do projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<VoleiApp.Data.VoleiContext>(opt => opt.UseInMemoryDatabase("VoleiDB"));
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
