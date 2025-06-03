c0d799a8dc95212700107c710330c8eda54531ca
Naum Santos Mourão <naum.santos@unemat.br>
2025-06-03 00:47:40 -0300

    Criação do DTO (Data Transfer Object) para resultado dos sorteios

---
+++ Models/SorteioResultDTO.cs
@@ -0,0 +1,17 @@
+﻿using VoleiApp.Models;
+
+/// <summary>
+/// Resultado do sorteio com os times e reservas.
+/// </summary>
+public class SorteioResultDTO
+{
+    /// <summary>
+    /// Times sorteados com seus atletas.
+    /// </summary>
+    public List<Time> Times { get; set; }
+
+    /// <summary>
+    /// Lista de atletas que ficaram como reservas.
+    /// </summary>
+    public List<Atleta> Reservas { get; set; }
+}
