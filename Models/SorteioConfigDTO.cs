dcc3568bb843dfe76073318946e939070dcd826d
Naum Santos Mourão <naum.santos@unemat.br>
2025-06-03 00:46:58 -0300

    Criação do DTO (Data Transfer Object) das configurações dos sorteios.

---
+++ Models/SorteioConfigDTO.cs
@@ -0,0 +1,17 @@
+﻿using VoleiApp.Models;
+
+/// <summary>
+/// Representa as configurações para sorteio de times.
+/// </summary>
+public class SorteioConfigDTO
+{
+    /// <summary>
+    /// Lista de atletas disponíveis para o sorteio.
+    /// </summary>
+    public List<Atleta> Atletas { get; set; }
+
+    /// <summary>
+    /// Quantidade de atletas por time (2, 4 ou 6).
+    /// </summary>
+    public int TamanhoDoTime { get; set; } = 4;
+}
