dce79b33110e16d431748689545069376bf8b5dd
Naum Santos Mourão <naum.santos@unemat.br>
2025-06-03 00:49:08 -0300

    Adiciona o CORS e configurações para documentação XML do swagger.

--- Program.cs
+++ Program.cs
@@ -6,10 +6,31 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        // Configuração do banco de dados em memória (para ambiente de desenvolvimento/testes)
         builder.Services.AddDbContext<VoleiApp.Data.VoleiContext>(opt => opt.UseInMemoryDatabase("VoleiDB"));
+
+        // Adiciona serviços ao contêiner (injeção de dependência)
         builder.Services.AddControllers();
+
+        // Configura Swagger (documentação da API)
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            // Permite gerar documentação XML se ativada no .csproj
+            var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            if (File.Exists(xmlPath))
+                options.IncludeXmlComments(xmlPath);
+        });
+
+        // CORS liberado para desenvolvimento (ajustar domínio em produção)
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("DevPolicy",
+                policy => policy.AllowAnyOrigin()
+                                .AllowAnyMethod()
+                                .AllowAnyHeader());
+        });
 
         var app = builder.Build();
 
@@ -19,6 +40,9 @@ internal class Program
             app.UseSwaggerUI();
         }
 
+        app.UseCors("DevPolicy");
+
+        app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
         app.Run();
