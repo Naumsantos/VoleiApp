329713a461f14d2614f86dabd27cfe5cfa0007f9
Naum Santos Mourão <naum.santos@unemat.br>
2025-06-03 00:46:10 -0300

    Criação do DTO (Data Transfer Object) para salvar as partidas.

---
+++ Models/SalvarPartidaDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace VoleiApp.Models
+{
+    /// <summary>
+    /// Representa uma requisição para salvar uma partida gerada pelo sorteio.
+    /// </summary>
+    public class SalvarPartidaDTO
+    {
+        public Time TimeA { get; set; }
+        public Time TimeB { get; set; }
+        public List<Atleta>? Reservas { get; set; }
+    }
+}
