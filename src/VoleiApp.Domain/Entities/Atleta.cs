0fb75a7a9a83bc368585b258f0b9f534aa646495
Naumsantos <naum_santos@hotmail.com>
2026-08-12 18:40:29 -0300

    feat(domain): rename using and initialization of properties

--- src/VoleiApp.Domain/Entities/Atleta.cs
+++ src/VoleiApp.Domain/Entities/Atleta.cs
@@ -1,9 +1,11 @@
-﻿namespace VoleiApp.Domain.Entities
+﻿using VoleiApp.Domain.Enums;
+
+namespace VoleiApp.Domain.Entities
 {
     public class Atleta
     {
         public int ID { get; set; }
-        public string Nome { get; set; }
+        public string Nome { get; set; } = string.Empty;
         public EPosicao Posicao { get; set; }
     }
 }
