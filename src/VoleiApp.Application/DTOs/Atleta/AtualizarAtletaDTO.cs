bef44d8abc3de89fe815a73434c4619f8e4fa85e
Naumsantos <naum_santos@hotmail.com>
2026-09-03 19:56:12 -0300

    feat(application): add atleta DTOS

---
+++ src/VoleiApp.Application/DTOs/Atleta/AtualizarAtletaDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace VoleiApp.Application.DTOs.Atleta
+{
+    public class AtualizarAtletaDTO
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Posicao { get; set; } = string.Empty;
+    }
+}
---
+++ src/VoleiApp.Application/DTOs/Atleta/CriarAtletaDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace VoleiApp.Application.DTOs.Atleta
+{
+    public class CriarAtletaDTO
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Posicao { get; set; } = string.Empty;
+    }
+}
